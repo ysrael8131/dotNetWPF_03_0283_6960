aed76eccf7cfbac0fa42424b89a1f06e2b50497d
Ysrael Rechtshafer <rechtsha@g.jct.ac.il>
2017-11-19 22:10:54 +0200

    Add project files.

---
+++ dotNetWPF_03_0283_6960/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace dotNetWPF_03_0283_6960
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
